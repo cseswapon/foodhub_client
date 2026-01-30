751938587b4992f71a177794783231bdcb12586e
cseswapon <cseswaponsaha@gmail.com>
2026-01-31 00:10:29 +0600

    [FIX] - fix error with oders page

--- src/app/(dashboardLayout)/@provider/provider/orders/page.tsx
+++ src/app/(dashboardLayout)/@provider/provider/orders/page.tsx
@@ -86,7 +86,7 @@ export default function ProviderOrder() {
       </div>
 
       {/* Orders Table Component */}
-      <OrdersTable orders={fakeOrdersData} />
+      <OrdersTable orders={fakeOrdersData} role="provider"/>
     </main>
   );
 }
