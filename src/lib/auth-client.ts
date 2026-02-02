514a2901803be798e431c92d59d152180d3a7f21
cseswapon <cseswaponsaha@gmail.com>
2026-02-02 15:06:17 +0600

    [ADD] - config file modify

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,16 @@
-import { env } from "@/env";
+/* import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_BACKEND_URL, // The base URL of your auth server
   credentials: "include",
+}); */
+
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: typeof window !== "undefined" ? window.location.origin : "",
+  basePath: "/api/auth",
+  fetchOptions: {
+    credentials: "include",
+  },
 });
