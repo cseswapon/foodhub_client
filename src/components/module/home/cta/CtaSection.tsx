ccee28da0655f3521941ffed4098b2b80867ae78
cseswapon <cseswaponsaha@gmail.com>
2026-05-03 23:28:00 +0600

    [MOD] - modify background color change with faq and cat section

--- src/components/module/home/cta/CtaSection.tsx
+++ src/components/module/home/cta/CtaSection.tsx
@@ -24,8 +24,8 @@ const whyUs = [
 
 export default function CtaSection() {
   return (
-    <section className="md:py-24 py-12 container mx-auto px-4 bg-linear-to-br from-[#0a0a0a] via-[#111] to-[#0a0a0a] text-white">
-      <div className="">
+    <section className="md:py-24 py-12 px-4 bg-linear-to-br from-[#0a0a0a] via-[#111] to-[#0a0a0a] text-white">
+      <div className="container mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
           {/* Left */}
           <motion.div
